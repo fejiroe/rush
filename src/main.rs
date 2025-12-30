fc79cbd37efa45840faa11ae1ad5aecd722a0c4c
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2025-12-30 03:32:55 +0000

    exit

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
-use std::io::Stdin;
+use std::io;
+use std::process;
 
 struct Shell {
     prompt: String,
     input: String,
+    // string optional for command 
+    // string optional vec for args
+    // builtins? 
 }
 
 impl Shell {
     fn printPrompt(&self) {println!("{}", &self.prompt);}
+    // parse cmds
+    // parse args
+    // check builtin
+    fn handleInput(&self) {
+        let _in = &self.input;
+        if (_in.contains("exit")) {
+            process::exit(0);
+        } else {print!("invalid command");}
+    }
 }
 
 fn main() {
     let mut shell = Shell {prompt: String::from("$"), input: String::new()};
-    let mut input = String::new();
-    while true {
+    loop {
         shell.printPrompt();
         io::stdin()
             .read_line(&mut shell.input)
             .expect("input error");
-        println!("invalid command");
+        shell.handleInput();
     }
 }
