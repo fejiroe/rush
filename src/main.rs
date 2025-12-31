5e95399e2a0dda5461bfda98f8cf886f7984dae2
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2025-12-31 23:17:56 +0000

    savepoint

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,75 @@
 use std::io;
-use std::process;
+use std::io::Error;
+use std::process::Command;
+
+/*
+fn getPathVar() {return std::env::var("HOME");}
+
+fn getCurrentDir() {return env::current_dir();}
+
+fn parseCmd(input: &str) -> Option<String> {
+    if input.is_empty() {
+        None
+    } else {
+    // else check for first space, return substring until space
+    return String::from(input)?;
+    }
+}
+
+fn parseArg(input: &str) -> Option<String> {
+    if input is empty return null
+    else check for first space, return substring after space
+}
+*/
+
+fn execCmd(input: &str) {
+    Command::new(input);
+}
+
+enum Builtins {Cd, Echo, Exit, Pwd, Type}
 
 struct Shell {
     prompt: String,
     input: String,
-    // string optional for command 
-    // string optional vec for args
-    // builtins? 
+    cmd: Option<String>,
+    arg: Option<String>,
+    path: Option<String>,
+    cwd: Option<String>,
 }
 
 impl Shell {
+    fn checkBuiltin(){}
     fn printPrompt(&self) {println!("{}", &self.prompt);}
-    // parse cmds
-    // parse args
-    // check builtin
+    fn printWorkingDir() {}
     fn handleInput(&self) {
-        let _in = &self.input;
-        if (_in.contains("exit")) {
-            process::exit(0);
-        } else {print!("invalid command");}
+        execCmd(&self.input);
+        // let cmd = parseCmd(&self.input);
+        // let arg = parseArg(&self.input);
+        // if not builtin execCmd(cmd) 
+        // else go through builtin
     }
 }
 
-fn main() {
-    let mut shell = Shell {prompt: String::from("$"), input: String::new()};
+fn main() -> Result <(), Error> {
+    let mut shell = Shell {
+        prompt: String::from("$"),
+        input: String::new(),
+        cmd: None,
+        arg: None,
+        path: None,
+        cwd: None};
     loop {
         shell.printPrompt();
         io::stdin()
             .read_line(&mut shell.input)
             .expect("input error");
         shell.handleInput();
+    } Ok (())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test] fn test1() {
     }
 }
